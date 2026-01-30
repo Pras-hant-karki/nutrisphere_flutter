344d5f47d4be67315e592b9d53983c21bd713173
Pras-hant-Karki <gymratprashant911@.com>
2026-01-31 00:11:01 +0545

    updated token service

--- lib/core/services/storage/token_service.dart
+++ lib/core/services/storage/token_service.dart
@@ -1,31 +1,52 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:nutrisphere_flutter/core/services/storage/user_session_service.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
-// provider
-final tokenServiceProvider = Provider<TokenService>((ref) {
-  return TokenService(prefs: ref.read(sharedPreferencesProvider));
-});
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class TokenService {
-  static const String _tokenKey = 'auth_token';
-  final SharedPreferences _prefs;
+  static const _tokenKey = 'auth_token';
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
 
-  TokenService({required SharedPreferences prefs}) : _prefs = prefs;
-
-  // Save token
   Future<void> saveToken(String token) async {
-    await _prefs.setString(_tokenKey, token);
+    await _storage.write(key: _tokenKey, value: token);
   }
 
-  // Get token
   Future<String?> getToken() async {
-    return _prefs.getString(_tokenKey);
+    return await _storage.read(key: _tokenKey);
   }
 
-  // Remove token (for logout)
-  Future<void> removeToken() async {
-    await _prefs.remove(_tokenKey);
+  Future<void> clearToken() async {
+    await _storage.delete(key: _tokenKey);
   }
 }
+
+
+
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:nutrisphere_flutter/core/services/storage/user_session_service.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
+
+// // provider
+// final tokenServiceProvider = Provider<TokenService>((ref) {
+//   return TokenService(prefs: ref.read(sharedPreferencesProvider));
+// });
+
+// class TokenService {
+//   static const String _tokenKey = 'auth_token';
+//   final SharedPreferences _prefs;
+
+//   TokenService({required SharedPreferences prefs}) : _prefs = prefs;
+
+//   // Save token
+//   Future<void> saveToken(String token) async {
+//     await _prefs.setString(_tokenKey, token);
+//   }
+
+//   // Get token
+//   Future<String?> getToken() async {
+//     return _prefs.getString(_tokenKey);
+//   }
+
+//   // Remove token (for logout)
+//   Future<void> removeToken() async {
+//     await _prefs.remove(_tokenKey);
+//   }
+// }
 
