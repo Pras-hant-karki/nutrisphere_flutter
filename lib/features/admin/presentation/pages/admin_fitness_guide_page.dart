712361aa777dda9ce4715d14537112d1b7019b1d
Pras-hant-Karki <gymratprashant911@.com>
2026-02-10 11:38:05 +0545

    created fitness page for admin and edited admin dashboard

---
+++ lib/features/admin/presentation/pages/admin_fitness_guide_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AdminFitnessGuidePage extends StatelessWidget {
+  const AdminFitnessGuidePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(child: Text('Admin Fitness Guide Page')),
+    );
+  }
+}
